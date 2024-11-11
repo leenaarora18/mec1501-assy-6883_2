42c97d13cbd1958833d4f86b270b534bd0777486
leenaarora18 <leenadarshan@gmail.com>
2024-11-11 10:29:59 +0530

    Simple Hello World Print

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,22 @@
+empty_file.o: /home/jayesh/zephyrproject/zephyr/misc/empty_file.c \
+ /home/jayesh/zephyrproject/zephyr/boards/microchip/mec15xxevb_assy6853/mec15xxevb_assy6853.dts \
+ /home/jayesh/zephyrproject/zephyr/boards/microchip/mec15xxevb_assy6853/mec15xxevb_assy6853-pinctrl.dtsi \
+ /home/jayesh/zephyrproject/zephyr/dts/arm/microchip/mec1501hsz.dtsi \
+ /home/jayesh/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/jayesh/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_macro.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_internal.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_loops.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_listify.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/dt-bindings/clock/mchp_xec_pcr.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/microchip-xec-gpio.h \
+ /home/jayesh/zephyrproject/zephyr/dts/arm/microchip/mec152x/mec152xhsz-pinctrl.dtsi \
+ /home/jayesh/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/mchp-xec-pinctrl.h
